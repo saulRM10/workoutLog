5d7649774114f59cfdbbd0acf1a79c38f27e877f
saulRM10 <saulreyes42315@gmail.com>
2021-10-28 12:44:24 -0700

    Intial commit

---
+++ app.js
@@ -0,0 +1,126 @@
+// template to creating a server in node js 
+// install npm  -> npm install 
+// instal express and body-parser -> npm install express body-parser
+// update server when there is a change -> nodemon nameofFile.js 
+
+const express = require("express");
+const bodyParcer = require("body-parser");
+const { setServers } = require("dns");
+
+const mongoose = require("mongoose");
+
+const app = express();
+
+// use ejs 
+app.set('view engine', 'ejs');
+
+// use body parcer 
+app.use(bodyParcer.urlencoded({extended:true }));
+
+//app.use(express.static("public"));// use these static elements (css, imgs etc )
+// need an array of items to store the to list items 
+
+let listofExr =[];
+
+// number of sets 
+let sets = 0;
+
+
+// lets use a database
+// 1) install mongoose -> npm i mongoose 
+// 2) require mongoose 
+// 3) connect to mongo
+mongoose.connect("mongodb://localhost:27017/ExerciseDB",{useNewUrlParser: true , useUnifiedTopology: true});
+
+// 4) create a database schema 
+
+const itemsSchema = {
+    name: String,
+    sets: String,
+    reps: String
+  };
+  // 5) cerate a mongoose model based on the schema 
+const Item = mongoose.model("Item", itemsSchema);
+
+
+  // 6) create new 'documents' == default items
+
+const item1 = new Item({
+    name: "bench",
+    sets: "4",
+    reps:"8"
+  });
+  
+  const item2 = new Item({
+    name: "squat",
+    sets: "3",
+    reps:"12"
+  });
+  
+  const item3 = new Item({
+    name: "overhead press ",
+    sets: "3",
+    reps:"8"
+
+  });
+
+  const defaultItems = [ item1, item2, item3];
+
+
+
+app.get("/", function(require, response){
+
+    let msg = "if there is a will, there is a way";
+
+    Item.find({}, function(err, foundItems){
+
+        if( foundItems.length === 0 ){
+            Item.insertMany(defaultItems,function(err){
+
+                if( err){
+                  console.log(err);
+                }else {
+                  console.log(" inserted default items into database");
+                }
+            } );
+            // we need to render items just created 
+        response.redirect("/");
+        } 
+        
+        else{
+            response.render('index', { grindMSG: msg , workout: foundItems });
+        }
+
+    })
+    
+ 
+    
+});
+
+
+app.post("/", function(require, response){
+
+    let exrName = require.body.newExr;
+
+    let NumSet = require.body.setNum;
+
+    let NumReps = require.body.repsNum;
+
+    // listofExr.push(exrName);
+
+   let myobj = { name: exrName, sets: NumSet, reps: NumReps };
+    
+    Item.insertMany(myobj, function(err, response) {
+        if (err) {
+            console.log("you did not add the item");
+        }
+        else { 
+            console.log("item inserted");
+        }
+    });
+    response.redirect("/");
+
+});
+app.listen(5000,function(){
+    console.log("connected to port 5000");
+});
