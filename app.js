45c93204334db97148d6cfaca9b0799263044292
saulRM10 <saulreyes42315@gmail.com>
2021-11-17 16:34:42 -0800

    items are updates but the updated item is not being rendered in the frontend

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const { setServers } = require("dns");
 
 const mongoose = require("mongoose");
 const { redirect } = require("statuses");
-const { all } = require("async");
+const { all, timeout } = require("async");
 
 const app = express();
 
@@ -75,8 +75,14 @@ const Log = mongoose.model("Log", logSchema);
   let openValueId; 
 
     let logNames =[];
+
+    let newItems = [];
+
+    // create a global variable to know what routine we are on;
+    let inthisRoutine = 'home' ; 
 // go home and render home page 
-app.get("/goHome", function(require, response){
+// /goHome
+app.get("/", function(require, response){
 
     Log.find({},{ WkName:1},{_id: 0},function(err, logNamesHere){
 
@@ -92,7 +98,7 @@ app.get("/goHome", function(require, response){
   });
 
 
-app.get("/", function(require, response){
+app.get("/timeout", function(require, response){
 
   
 
@@ -108,13 +114,13 @@ app.get("/", function(require, response){
                 }
             } );
             // we need to render items just created 
-        response.redirect("/");
+        response.redirect("/" + inthisRoutine);
         } 
         
-        else{
-            response.render('index', { routineName: "logA" , workout: foundItems, OpenEditId: openValueId });
+       // else{
+         //   response.render('index', { routineName: "logA" , workout: foundItems, OpenEditId: openValueId });
 
-        }
+        //}
 
     })
     
@@ -126,20 +132,23 @@ app.get("/", function(require, response){
 app.post("/newpage", function(require,response){
 
     const pageName= require.body.newpageName;
+    //inthisRoutine= pageName;
 
     response.redirect("/"+ pageName);
 });
 
 // create new workout log, named whatever you want
 app.get("/:customLogName", function(require,response){
-
+    
     const customLogName = require.params.customLogName;
 
+    // use this variable, to know what log we are in at all times. 
+      inthisRoutine = customLogName;
       // need to check if a 'log' of the same name already exist 
       Log.findOne({ WkName:customLogName}, function(err , foundLogs){
 
           if(!err){
-            // if log does not exist if foundLogs, create one 
+            // if log  under the customLogName does not exist if foundLogs, create one 
               if( !foundLogs ){
                
                 Log.insertMany([{ WkName:customLogName , logs: defaultItems}],function(err){
@@ -151,20 +160,24 @@ app.get("/:customLogName", function(require,response){
                   }else {
                     console.log(" inserted default items into database");
                   }
+                  
               } );
+
+              // need to render/check for deleted items => exName, sets , reps, weight 
+             
               // we need to render items just created 
-          response.redirect("/"+customLogName);
+          response.redirect("/"+ customLogName);
               }
               else{
                 // display the existing log, that can be found in foundLogs
-                response.render('index', { routineName: foundLogs.WkName , workout: foundLogs.logs, OpenEditId: null  });
+                response.render('index', { routineName: foundLogs.WkName , workout: foundLogs.logs, OpenEditId: openValueId  });
               }
           }
       })
 });
 
 
-app.post("/", function(require, response){
+app.post("/createItem", function(require, response){
 
     let exrName = require.body.newExr;
 
@@ -176,8 +189,6 @@ app.post("/", function(require, response){
     let wght = require.body.weight;
 
     // create an array of weight to store the weight 
-   
-
      let weightDatastring =[];
      weightDatastring = wght.split(',');
 
@@ -190,33 +201,34 @@ app.post("/", function(require, response){
 
  
 
-   if( whatRoutine === "logA"){
-    Item.insertMany(myobj, function(err, response) {
-      if (err) {
-          console.log("you did not add the item");
-      }
-      else { 
-          console.log("item inserted");
-      }
-  });
+//    if( whatRoutine === "logA"){
+//     Item.insertMany(myobj, function(err, response) {
+//       if (err) {
+//           console.log("you did not add the item");
+//       }
+//       else { 
+//           console.log("item inserted");
+//       }
+//   });
 
-  response.redirect("/");
+//   response.redirect("/");
 
     
-}
-else{
+// }
+// else{
 
 //Need to see what routine this myObj belongs too 
- Log.findOne({WkName: whatRoutine}, function(err, foundLogs){
+  Log.findOne({WkName: whatRoutine}, function(err, foundLogs){
 
-   
-  // tap in to found logs, tap in to items, push myobj into array of items (items = exercise + sets + reps + weight ) 
-   foundLogs.logs.push(myobj);
+    
+    // tap in to found logs, tap in to items, push myobj into array of items (items = exercise + sets + reps + weight ) 
+    foundLogs.logs.push(myobj);
 
-    foundLogs.save();
-    // render the new item in the routine it belongs too 
-    response.redirect("/" + whatRoutine);
- });
+      foundLogs.save();
+      // render the new item in the routine it belongs too 
+      response.redirect("/" + whatRoutine);
+  });
+});
   
 
 // this is how to update an item from a different log 
@@ -236,9 +248,9 @@ else{
 // ^ update code 
 
 
-}
+//}
 
-});
+
 
 app.post("/deleteLog", function(require, response){
 
@@ -251,7 +263,7 @@ app.post("/deleteLog", function(require, response){
         console.log("log has been deleted successfully");
     }
     // deleted the item, not go back to root and render what we do have left
-    response.redirect("/goHome");
+    response.redirect("/");
     });
 });
 
@@ -264,10 +276,10 @@ app.post("/delete", function(require, response ){
 
     Item.deleteOne({_id: noMore}, function(err){
         if( !err){
-            console.log("item has been deleted successfully");
+            console.log("item has been deleted successfully: " + noMore);
         }
         // deleted the item, not go back to root and render what we do have left
-        response.redirect("/");
+        response.redirect("/"+ inthisRoutine);
     });    
 });
 
@@ -287,33 +299,42 @@ app.post("/update", function(require,response){
 
   
 
-      var myquery = {  _id: updateItem };
+     var myquery = { WkName: inthisRoutine ,  _id: updateItem };
       var newValues = { $set: {name: newName, sets: newSetNum, reps: newRepNum, weight: NewWeightDatastring  } };
       //let userInput = response.body.newItemData; 
 
-       Item.updateOne(myquery, newValues, function(err, response) {
-        if( !err){
-
-            console.log("item has been updated successfully for item:" + updateItem );
-            
-        }
-
-      });
       // updated the item, not go back to root and render what we do have left
-    response.redirect("/");
+  Log.updateOne( myquery,newValues,function(err, response) {
+
+    if(!err){
+              console.log("item updated in LOG: " + inthisRoutine + " is now : " + newName );
+            }
+           // response.redirect("/" + inthisRoutine );
+
+           // updated the item now push it ?
+          
+        });
+
+        
+    response.redirect("/" + inthisRoutine );
+
+  
+
+    
 });
 
 
 // gets 
 app.post("/openId", function(require, response){
 
+  // i am on this item, item I want to edit ( name, reps, sets, weight)
    openValueId = require.body.editBtn;
-
+    
  
   console.log("this is the name of the item clicked on: " + openValueId);
 
- 
-    response.redirect("/");
+ // here is the edit 
+    response.redirect("/" + inthisRoutine);
 
 
 });
@@ -322,7 +343,8 @@ app.post("/openId", function(require, response){
 app.post("/close", function(require,response){
 
   openValueId = null; 
-  response.redirect("/");
+  // go to the specific page you are doing the edit;
+  response.redirect("/" + inthisRoutine);
   
 });
 
