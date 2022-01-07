b83f2de1d00fb6728f8ea01269c659907b1b2660
saulRM10 <43281985+saulRM10@users.noreply.github.com>
2022-01-07 09:24:34 -0800

    not going to use satatic files

--- public/main.js
+++ public/main.js
@@ -1,3 +1,9 @@
-
-
-alert("This is an alert message box.");  // display string message
+console.log( "main.js => routine name: " + routine.name);
+console.log(" main.js => new name : " + newPageName);
+// if name of routine created already exist 
+if ( routine.name == newPageName ){
+    alert("this routine already exist, type a different name");
+}
+// show alert 
+// ask them to enter a different name 
+//alert("This is an alert message box.");  // display string message
